7ee12d66429fddc1e5f9a9bb9ef4422810feb680
taka <tomo05061@gmail.com>
2024-03-16 22:49:49 +0900

    バーコード生成処理を追加

--- src/app/api/v1/barcode/route.ts
+++ src/app/api/v1/barcode/route.ts
@@ -1,10 +1,33 @@
+import { z } from 'zod';
+import { createCanvas } from 'canvas';
+import JsBarcode from 'jsbarcode';
 import { apiHandler } from '@/app/api/helpers/handler';
+
+export type BarResponse = Blob;
+
+const generateQrSchema = z.object({
+  code: z.string().nullish(),
+});
+
+const generateBarCode = (data: string, edgeSize = 150) => {
+  const canvas = createCanvas(edgeSize, edgeSize);
+  const options: JsBarcode.Options = {
+    format: "EAN13",
+    flat: true,
+    height: 60,
+  };
+  JsBarcode(canvas, data, options);
+  return canvas.toDataURL();
+};
 
 export const GET = apiHandler.create({
   authentication: false,
-  handler: async () => {
-    return {
-        statusMsg: "Generate Barcode"
-    };
+  inputSchema: generateQrSchema,
+  handler: async ({ session, input }): Promise<BarResponse> => {
+    const code = input.code || '123456789012';
+    const barCodeDataUrl = generateBarCode(code);
+    const barCodeBlob = (await fetch(barCodeDataUrl)).blob();
+
+    return barCodeBlob;
   },
 });
